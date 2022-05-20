b59dc5418f7f588c6c5e18ab1e48192258ebb2c0
Corinna Raimondo <corinna.raimondo@gmail.com>
2022-05-19 21:04:42 -0500

    edits to merge my bbranch to main

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 const apiRouter = require('express').Router();
+const { JWT_SECRET } = process.env;
+const { getUserById } = require('../db/models/user');
+const jwt = require('jsonwebtoken');
+
 const {
   createProduct,
   getAllProducts
 } = require('../db/models/products')
+
+
+
 
 apiRouter.get('/', (req, res, next) => {
   res.send({
@@ -15,6 +22,45 @@ apiRouter.get('/health', (req, res, next) => {
     healthy: true,
   });
 });
+
+
+
+apiRouter.use(async (req, res, next) => {
+	const prefix = 'Bearer ';
+	const auth = req.header('Authorization');
+
+	if (!auth) {
+		next();
+	} else if (auth.startsWith(prefix)) {
+		const token = auth.slice(prefix.length);
+
+		try {
+			const { id } = jwt.verify(token, JWT_SECRET);
+			if (id) {
+				req.user = await getUserById(id);
+				next();
+			}
+		} catch ({ name, message }) {
+			next({ name, message });
+		}
+	} else {
+		next({
+			name: 'AuthorizationHeaderError',
+			message: `Authorization token must start with ${prefix}`
+		});
+	}
+});
+
+
+
+apiRouter.use((req, res, next) => {
+	if (req.user) {
+		console.log('User is set:', req.user);
+	}
+	next();
+});
+
+
 
 // place your routers here
 apiRouter.post('/products', async (req, res, next) => {
@@ -34,5 +80,15 @@ apiRouter.get('/products', async (req, res, next) => {
     next(error);
   }
 })
+
+
+
+
+
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
+
+
+
 
 module.exports = apiRouter;
