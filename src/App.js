a395b189f58a08c5ad32d09bbeac6e61fc6eb916
T2ayala <tonyayala02@gmail.com>
2022-06-04 09:52:32 -0500

    working styling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@
 // // you can think of that directory as a collection of api adapters
 // // where each adapter fetches specific info from our express server's /api route
 
-import { getAPIHealth, retrieve, add, update, remove, emptyCart, capture, refreshCart } from './axios-services';
-//import '../style/App.css'
-// newStuff
+import { getAPIHealth, getAllProducts, retrieve, add, update, remove, emptyCart, capture, refreshCart } from './axios-services';
+// import '../style/App.css'
 import React, { useState, useEffect } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Products, Cart } from './components';
-// import { get } from 'express/lib/response';
-// import { getAllProducts } from './db/models/product';
-//import our product source
+
 const App = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [products, setProducts] = useState([]);
@@ -26,11 +23,11 @@ const App = () => {
   testHealth();
 
 // after each "await" we need to add where our product source
-  // const fetchProducts = async () => {
-  //   const { data } = await getAllProducts();
+  const fetchProducts = async () => {
+    const { data } = await getAllProducts();
 
-  //   setProducts(data);
-  // };
+    setProducts(data);
+  };
 
   const fetchCart = async () => {
     setCart(await retrieve());
@@ -62,18 +59,18 @@ const App = () => {
   };
 
 
-  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
-    try {
-      const incomingOrder = await capture(checkoutTokenId, newOrder);
+  // const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+  //   try {
+  //     const incomingOrder = await capture(checkoutTokenId, newOrder);
 
-      setOrder(incomingOrder);
+  //     setOrder(incomingOrder);
 
-      refreshCart();
-    } catch (error) {
-      setErrorMessage(error.data.error.message);
-    }
-  };
-// end of "await" adding
+  //     refreshCart();
+  //   } catch (error) {
+  //     setErrorMessage(error.data.error.message);
+  //   }
+  // };
+
   // useEffect(() => {
   //   fetchProducts();
   //   fetchCart();
